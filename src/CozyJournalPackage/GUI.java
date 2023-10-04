fa7f89ac61a4c1947fa4cecf46dc4910f5414451
tinninj <tinninj4@gmail.com>
2023-10-03 21:32:29 -0500

    Updated the GUI and added images to be used for the navigation panel

--- src/CozyJournalPackage/GUI.java
+++ src/CozyJournalPackage/GUI.java
@@ -2,95 +2,102 @@ package CozyJournalPackage;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
-public class GUI implements ActionListener{
-	//add JFrame
-	private JFrame frame;
-	//add JPanel
-	private JPanel panel;
-	//add JButton
-	private JButton button;
-	//add JLable
-	private JLabel label;
-	
-	public GUI(){
-		
-				
-		//creates frame
-		frame = new JFrame();
-		//set title of frame
-		frame.setTitle("Cozy Journal");
-		//exit out of application
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//prevent frame from being resized
-		frame.setResizable(false);
-		//set size of frame(x an y)
-		frame.setSize(780,780);
-		//make frame visible
-		frame.setVisible(true);
-		//change background color of frame
-		frame.getContentPane().setBackground(new Color(252,252,202,255));
-		
-		//will change this later
-		frame.setLayout(null);
-				
-		/*Change the icon for the GUI(NOT WORKING!)
-		* ImageIcon image = new ImageIcon("notebook.png");
-		* frame.setIconImage(image.getImage());
-		*/
-				
-		
-				
-		//creates panel
-		JPanel titlePanel = new JPanel();
-		titlePanel.setBackground(new Color(252,252,202,255));
-		titlePanel.setBorder(BorderFactory.createLineBorder(new Color(143,96,70,255)));
-		titlePanel.setBounds(0,0,780,50);
-		
-		JPanel centerPanel = new JPanel();
-		centerPanel.setBackground(new Color(252,252,202,255));
-		centerPanel.setBorder(BorderFactory.createLineBorder(new Color(143,96,70,255)));
-		centerPanel.setBounds(100,100,550,400);
-		
-		JPanel moodPanel = new JPanel();
-		moodPanel.setBackground(new Color(252,252,202,255));
-		moodPanel.setBorder(BorderFactory.createLineBorder(new Color(143,0,70,255)));
-		moodPanel.setBounds(0,600,780,50);
-		
-		JPanel navigationPanel = new JPanel();
-		navigationPanel.setBackground(new Color(252,252,202,255));
-		//navigationPanel.setBorder(BorderFactory.createLineBorder(new Color(143,0,70,255)));
-		navigationPanel.setBounds(0,650,780,350);
-	
+public class GUI {
+    private JFrame frame;
 
-		
-		//add panels to the frame
-		frame.add(titlePanel);
-		frame.add(centerPanel);
-		frame.add(moodPanel);
-		frame.add(navigationPanel);
-	}
+    public GUI() {
+		//Creates frame and sets dimensions/attributes
+        frame = new JFrame();
+        frame.setTitle("Cozy Journal");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setSize(780, 780);
+        frame.setVisible(true);
 
-	public static void main(String[] args) {
-		
-		new GUI();
-		
-	}
+		//Adds navigation to the bottom and adds to frame
+        JTabbedPane navigationPane = new JTabbedPane(JTabbedPane.BOTTOM);
+		frame.add(navigationPane);
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
 
+		//Creates pages of the app
+        JPanel mainPage = createPage("Home");
+        JPanel journalPage = createJournalPage("Journal");
+        JPanel calendarPage = createPage("Calendar");
+        JPanel analyticsPage = createPage("Analytics");
+
+        //Add pages to navigation 
+		navigationPane.addTab("Home", mainPage);
+        navigationPane.addTab("Journal", journalPage);
+        navigationPane.addTab("Calendar", calendarPage);
+        navigationPane.addTab("Analytics", analyticsPage);
+
+        
+    }
+
+    private JPanel createPage(String title) {
+        JPanel page = new JPanel();
+        JPanel titlePanel = new JPanel();
+        JPanel centerPanel = new JPanel();
+
+        page.setLayout(new BorderLayout());
+        page.add(titlePanel, BorderLayout.NORTH);
+        page.add(centerPanel, BorderLayout.CENTER);
+
+        titlePanel.setPreferredSize(new Dimension(780, 50));
+		titlePanel.setBackground(new Color(252, 252, 202));
+		titlePanel.setBorder(BorderFactory.createLineBorder(new Color(143, 96, 70)));
+
+        centerPanel.setBackground(new Color(252, 252, 202));
+
+        JLabel titleLabel = new JLabel(title);
+        titleLabel.setForeground(new Color(143, 96, 70));
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        titlePanel.add(titleLabel);
+
+        return page;
+    }
+
+	//method to create Journal Page
+    private JPanel createJournalPage(String title) {
+        JPanel page = createPage(title);
+        JPanel centerPanel = new JPanel();
+        JPanel leftPanel = new JPanel();
+        JPanel rightPanel = new JPanel();
+
+		//SetLayout: 1 row, 2 columns
+        centerPanel.setLayout(new GridLayout(1, 2));
+        centerPanel.add(leftPanel);
+        centerPanel.add(rightPanel);
+
+		//Add to page
+        page.add(centerPanel, BorderLayout.CENTER);
+
+		//Set background Color
+        leftPanel.setBackground(new Color(252, 252, 202));
+        rightPanel.setBackground(new Color(252, 252, 202));
+
+
+        leftPanel.setBorder(BorderFactory.createLineBorder(new Color(143, 96, 70)));
+        rightPanel.setBorder(BorderFactory.createLineBorder(new Color(143, 96, 70)));
+
+        // Adjust the size of the left and right panels
+        leftPanel.setPreferredSize(new Dimension(200, 400));
+        rightPanel.setPreferredSize(new Dimension(400, 400));
+
+        return page;
+    }
+
+    public static void main(String[] args) {
+        new GUI();
+    }
 }
