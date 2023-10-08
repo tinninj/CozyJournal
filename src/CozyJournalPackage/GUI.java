bae589f4d1882d23858e8c3099f9347e57e166f5
tinninj <tinninj4@gmail.com>
2023-10-07 21:43:46 -0500

    added a few changes

--- src/CozyJournalPackage/GUI.java
+++ src/CozyJournalPackage/GUI.java
@@ -15,6 +15,13 @@ import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+import java.io.FileOutputStream; 
+import java.io.IOException; 
+import org.apache.poi.ss.usermodel.Cell; 
+import org.apache.poi.ss.usermodel.Row; 
+import org.apache.poi.xssf.usermodel.XSSFSheet; 
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
+
 public class GUI {
     private JFrame frame;
 
